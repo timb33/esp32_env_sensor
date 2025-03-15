5832cfc32e043f3c961492a8f6fa0e68df4776d2
timb33 <timdbrown@yahoo.co.uk>
2025-03-15 13:58:33 +0000

    improve logging

--- humidity_pub_noscreen.ino
+++ humidity_pub_noscreen.ino
@@ -15,15 +15,10 @@ const char* ssid = "";
 const char* password = "";
 
 // MQTT broker details
-const char* mqtt_server = "broker.local";
+const char* mqtt_server = "192.168.0.82";//"broker.hivemq.com";
 const int mqtt_port = 1883;
 const char* mqtt_user = ""; // Anonymous access
 const char* mqtt_password = "";
-
-
-
-// OLED display setup (I2C address: 0x3C, pins: GPIO 5 (SCL), GPIO 4 (SDA))
-//SSD1306 display(0x3C, 5, 4);
 
 // BMP280 setup
 Adafruit_BMP280 bmp; // I2C interface (default address: 0x76 or 0x77)
@@ -32,9 +27,9 @@ Adafruit_BMP280 bmp; // I2C interface (default address: 0x76 or 0x77)
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// setup telegraph/morse
 const int pinLED = 15; // Pin connected to the LED
 
+// setup telegraph/morse
 Telegraph telegraph(pinLED, 5, HIGH);
 
 String deviceName;
@@ -45,10 +40,9 @@ void setupWiFi() {
   Serial.println("Connecting to Wi-Fi...");
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
+    delay(1000); Serial.print(".");
   }
-  Serial.println("\nWi-Fi connected!");
+  telegraph.send("gggggggg");// --.   Serial.println("\nWi-Fi connected!");
 }
 
 // Function to connect to MQTT broker
@@ -58,7 +52,7 @@ void connectToMQTT() {
     if (client.connect("ESP32_Humidity_Sensor", mqtt_user, mqtt_password)) {
       Serial.println("Connected to MQTT!");
     } else {
-      Serial.print("Failed MQTT connection. State: ");
+      telegraph.send("ffffffff");// ..-.       Serial.print("Failed MQTT connection. State: ");
       Serial.println(client.state());
       delay(2000);
     }
@@ -75,16 +69,8 @@ String getDeviceName() {
 void setup_spoof() {
   Serial.begin(115200);
   pinMode(pinLED, OUTPUT);
-
-  for (int i = 0; i < 3; i++){
-    //digitalWrite(15, HIGH);delay(1000);digitalWrite(15, LOW);delay(1000); 
-  }
-
-  // Initialize Wi-Fi
-  setupWiFi();
-
-  // Initialize MQTT
-  client.setServer(mqtt_server, mqtt_port);
+  setupWiFi();// Initialize Wi-Fi
+  client.setServer(mqtt_server, mqtt_port);// Initialize MQTT
   connectToMQTT();
 }
 
@@ -95,30 +81,12 @@ void setup() {
     // Initialize I2C with explicit pins: SDA = GPIO35, SCL = GPIO33
   Wire.begin(35, 33);//s(erial)da(ta), s(erial)cl(ock)
 
-  telegraph.send("aaaaaaaa"); //.-
-
-  // Initialize OLED display
-  // display.init();
-  // display.setFont(ArialMT_Plain_24);
-  // display.setTextAlignment(TEXT_ALIGN_CENTER);
-  // display.clear();
-  // display.drawStringMaxWidth(64, 20, 128, "Initializing...");
-  // display.display();
-
-  // delay(2000);
-  // display.clear();
-  // display.drawStringMaxWidth(64, 20, 128, "Initialized.");
-  // display.display();
+  telegraph.send("aaaaaaaa"); //.-  in setup
 
   // Initialize BMP280 sensor
   if (!bmp.begin(0x76)) { // Default I2C address
-    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
-    //display.clear();
-    //display.drawStringMaxWidth(64, 20, 128, "BMP280 Error!");
-    //display.display();
-
-    telegraph.send("bbbbbbbb");//-...
-
+    
+    telegraph.send("bbbbbbbb");// -... Serial.println("Could not find a valid BMP280 sensor, check wiring!");
     while (1); // Stop execution
   }
 
@@ -131,9 +99,6 @@ void setup() {
 
   // Initialize Wi-Fi
   setupWiFi();
-  // display.clear();
-  // display.drawStringMaxWidth(64, 20, 128, "WiFi connected.");
-  // display.display();
 
   // Initialize time with NTP
   configTime(0, 0, "pool.ntp.org", "time.nist.gov"); // Use UTC (offset = 0, DST = 0)
@@ -143,34 +108,27 @@ void setup() {
   while (!time(nullptr)) {
     delay(500);
     Serial.print(".");
-    telegraph.send("cccccccc");//-.-.
+    telegraph.send("cccccccc");// -.-. setting up time
   }
-  Serial.println("Time synchronized.");
+  
+  telegraph.send("hhhhhhhh");// .... Serial.println("Time synchronized.");
 
   // Initialize MQTT
   client.setServer(mqtt_server, mqtt_port);
   connectToMQTT();
-  // display.clear();
-  // display.drawStringMaxWidth(64, 20, 128, "MQTT connected.");
-  // display.display();
 
-  // Clear display after initialization
-  // display.clear();
-  // display.drawStringMaxWidth(64, 20, 128, "Ready!");
-  // display.display();
-
-    // Get unique device name
   deviceName = getDeviceName();
-
-  // MQTT topic
   mqtt_topic = "home/sensors999/" + deviceName + "/data";
 }
 
 void loop() {
+  telegraph.send("eeeeeeee");// .      in main loop 
   if (!client.connected()) {
     connectToMQTT();
   }
   client.loop();
+  telegraph.send("iiiiiiii");// ..   mqtt connected 
+
 
   // Get the current time in Zulu/UTC format
   time_t now;
@@ -189,20 +147,8 @@ void loop() {
   snprintf(payload, sizeof(payload), "{\"temperature\": %.2f, \"pressure\": %.2f, \"timestamp\": \"%s\"}", temp_c, pressure_h, time_str);
   client.publish(mqtt_topic.c_str(), payload);
 
-  // Display data on the OLED
-  // display.clear();
-  // display.drawStringMaxWidth(64, 10, 128, "Humidity:");
-  // display.drawStringMaxWidth(64, 40, 128, String(temp_c) + " %");
-  // display.display();
+  // Debug output to Serial Monitor   Serial.print("Published fake humidity data: ");   Serial.println(payload);
 
-  // Debug output to Serial Monitor
-  Serial.print("Published fake humidity data: ");
-  Serial.println(payload);
-
-    telegraph.send("dddddddd");//-..
-  // for (int i = 0; i < 20; i++){
-  //   digitalWrite(15, HIGH);delay(500);digitalWrite(15, LOW);delay(500); 
-  // }
-
+  telegraph.send("dddddddd");// -.. end of main loop 
   // Send data every 10 seconds
 }
